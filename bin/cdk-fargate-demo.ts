294aedae72cda0a4e70063a8b02a7380c67d3edd
OAwan <georgiowan@cloudwaretw.com>
2020-07-07 16:45:01 +0800

    cdk build tg, elb, taskRole, fargateTaskDef, fargateService

--- bin/cdk-fargate-demo.ts
+++ bin/cdk-fargate-demo.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
+import * as readlineSync from 'readline-sync';
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkFargateDemoStack } from '../lib/cdk-fargate-demo-stack';
 
+if (!process.env['ENV_ACCOUNT'] || !process.env['ENV_REGION']) {
+    process.env['ENV_ACCOUNT'] = readlineSync.question('> 請輸入 aws account id: ');
+    process.env['ENV_REGION'] = readlineSync.question('> 請輸入 stack 建立之 region (default: ap-northeast-1): ') || 'ap-northeast-1';
+}
+
 const app = new cdk.App();
-new CdkFargateDemoStack(app, 'CdkFargateDemoStack');
+new CdkFargateDemoStack(app, 'CdkFargateDemoStack', {
+    env: {
+        account: process.env['ENV_ACCOUNT'],
+        region: process.env['ENV_REGION']
+    }
+});
